27b9c4c25aa891e3f5fa665f4b552dbb2b490e21
Dan Smith <dan.smith.me@gmail.com>
2018-08-26 09:12:08 +0200

    Added simple templates for views, defined urls
    Created simple templates using the django templating system.
    Defined some SEO friendly urls.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
+    """A view that returns a rendered list of published posts."""
     posts = Post.published.all()
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
-
+def post_detail(request, year, month, day, post):
+    """A view that renders a posts detail if the request is valid.
+    otherise it returns a 404 page."""
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
