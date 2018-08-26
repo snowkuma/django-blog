d65947348d9f36959c5e7b7e0425b15326a0b1ab
Dan Smith <dan.smith.me@gmail.com>
2018-08-26 16:19:03 +0200

    Added pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 def post_list(request):
-    """A view that returns a rendered list of published posts."""
-    posts = Post.published.all()
+    """A view that returns a rendered list of published posts.
+    implements the django pagination class to split the list automatically
+    across several pages.
+    """
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts per page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver the last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'page': page,
+                   'posts': posts})
 
 def post_detail(request, year, month, day, post):
     """A view that renders a posts detail if the request is valid.
