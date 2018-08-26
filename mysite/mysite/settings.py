df6f451c182488a2c64b492fc34d430a696508af
Dan Smith <dan.smith.me@gmail.com>
2018-08-26 18:21:43 +0200

    Added share by email functionality

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Write emails to shell (useful for testing)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
