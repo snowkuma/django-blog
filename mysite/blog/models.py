3c68cd991ad88386124fa446843c0c3caef5f903
Dan Smith <dan.smith.me@gmail.com>
2018-08-28 09:40:45 +0100

    Added the simple comment system

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -46,3 +46,24 @@ class Post(models.Model):
                              self.slug])
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # enable us to deactivate inappropriate comments
+    active = models.BooleanField(default=True)
+
+
+    class Meta:
+        ordering = ('created',)
+    
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
+
+
