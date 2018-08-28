8941debc647d685b2358cddeda8da054efbf4d05
Dan Smith <dan.smith.me@gmail.com>
2018-08-28 11:40:11 +0100

    Added simple reccomender system.

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ Installed via pip
 
 # Added filter by tag.
 
+# Retrieve posts by similarity.
+
+By adding a count we can find similar posts based onThe number of tags they share.
+
+1. Retrieve all tags for current post
+2. Get all posts that are tagged with any of those tags
+3. Exclude the current post from that list to avoid recommending same post
+4. OrderThe results byThe number of tags shared with the current post
+5. In a case where there is a tie, recommend the most recent post
+6. Limit the query to the number of posts we want to recommend
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -36,6 +37,7 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         # If page is out of range deliver the last page of results
         posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
@@ -71,12 +73,21 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    
+    # List ofSimilar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)    
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                    .order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
